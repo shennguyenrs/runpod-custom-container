c5bf3137f092e695bb70687dbd79004062e77c7c
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2023-09-03 14:16:14 +0300

    feat: update webui, controlnet, link script

---
+++ sd-a1111-runtime/create-links.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -sf /cn-models/* /workspace/stable-diffusion-webui/models/ControlNet/
+ln -sf /sd-lora/* /workspace/stable-diffusion-webui/models/Lora/
+ln -sf /sd-models/* /workspace/stable-diffusion-webui/models/Stable-diffusion/
+ln -sf /sd-vae/* /workspace/stable-diffusion-webui/models/VAE/
+ln -sf /sd-upscaler/* /workspace/stable-diffusion-webui/models/ESRGAN/
+ln -sf /sd-extensions/* /workspace/stable-diffusion-webui/extensions/
+ln -sf /sd-embeddings/* /workspace/stable-diffusion-webui/embeddings/
