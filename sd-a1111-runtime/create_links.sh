64d2719f0650644e6e814becdbd308216ba01ac8
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2023-09-08 20:46:31 +0300

    feat: script to create links, update webui

---
+++ sd-a1111-runtime/create_links.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/sh
+
+ln -s /cn-models/* /workspace/stable-diffusion-webui/models/ControlNet/
+ln -s /sd-lora/* /workspace/stable-diffusion-webui/models/Lora/
+ln -s /sd-models/* /workspace/stable-diffusion-webui/models/Stable-diffusion/
+ln -s /sd-vae/* /workspace/stable-diffusion-webui/models/VAE/
+ln -s /sd-upscaler/* /workspace/stable-diffusion-webui/models/ESRGAN/
+ln -s /sd-extensions/* /workspace/stable-diffusion-webui/extensions/
+ln -s /sd-embeddings/* /workspace/stable-diffusion-webui/embeddings/
--- sd-a1111-runtime/pre_start.sh
+++ sd-a1111-runtime/pre_start.sh
@@ -4,14 +4,6 @@ rsync --remove-source-files -rlptDu --ignore-existing /venv/ /workspace/venv/
 echo "**** syncing stable diffusion to workspace, please wait ****"
 rsync --remove-source-files -rlptDu --ignore-existing /stable-diffusion-webui/ /workspace/stable-diffusion-webui/
 
-ln -s /cn-models/* /workspace/stable-diffusion-webui/models/ControlNet/
-ln -s /sd-lora/* /workspace/stable-diffusion-webui/models/Lora/
-ln -s /sd-models/* /workspace/stable-diffusion-webui/models/Stable-diffusion/
-ln -s /sd-vae/* /workspace/stable-diffusion-webui/models/VAE/
-ln -s /sd-upscaler/* /workspace/stable-diffusion-webui/models/ESRGAN/
-ln -s /sd-extensions/* /workspace/stable-diffusion-webui/extensions/
-ln -s /sd-embeddings/* /workspace/stable-diffusion-webui/embeddings/
-
 if [[ $RUNPOD_STOP_AUTO ]]
 then
     echo "Skipping auto-start of webui"
